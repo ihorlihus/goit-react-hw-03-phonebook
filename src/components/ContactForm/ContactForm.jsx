fc17db4ad828c4ed7ef924ca9b4aa01d9fe4ef50
ihorlihus <ligusigor@ukr.net>
2022-08-19 16:02:08 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,19 +7,23 @@ const ContactFormWrap = styled.form`
   flex-direction: column;
   gap: ${props => props.theme.space[2]}px;
   margin-left: ${p => p.theme.space[1]}px;
-  border: 2px solid #000000;
-  padding: 8px 8px;
-  border-radius: 4px;
-  margin-bottom: 4px;
+  border: ${p => p.theme.space[1]}px solid ${p => p.theme.colors.black};
+  padding: ${p => p.theme.space[3]}px ${p => p.theme.space[3]}px;
+  border-radius: ${p => p.theme.space[2]}px;
+  margin-bottom: ${p => p.theme.space[2]}px;
   box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
     1px 4px 6px rgba(0, 0, 0, 0.16);
 
   input:focus,
   input:hover {
     outline: none;
-    border: 2px solid ${p => p.theme.colors.primary};
+    border: ${p => p.theme.space[1]}px solid ${p => p.theme.colors.primary};
   }
 `;
+
+const   = styled.button`
+  
+`
 class ContactForm extends Component {
   state = {
     name: '',
